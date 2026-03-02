4930c72b2afa9273545f2190846773bf95ed0390
Miguel Vega Perez <aries_242015@hotmail.com>
2026-03-02 16:40:27 -0500

    primer commit

---
+++ src/test/java/com/mrdevv/portfolioBackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdevv.portfolioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
