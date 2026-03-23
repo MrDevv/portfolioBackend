3ac59a67899d8aa97729451a9a0048ff1c649b52
Miguel Angel Vega Perez <aries_242015@hotmail.com>
2026-03-23 17:41:51 -0500

    feat: agregar endpoint para obtener las experiencias del desarrollador mediante su API KEY

--- src/main/java/com/mrdevv/portfolioBackend/controllers/DesarrolladorController.java
+++ src/main/java/com/mrdevv/portfolioBackend/controllers/DesarrolladorController.java
@@ -1,15 +1,17 @@
 package com.mrdevv.portfolioBackend.controllers;
 
+import com.mrdevv.portfolioBackend.dto.ResponseWithPageable;
 import com.mrdevv.portfolioBackend.dto.response.ResponseDesarrolladorDTO;
+import com.mrdevv.portfolioBackend.dto.response.ResponseExperienciasDTO;
 import com.mrdevv.portfolioBackend.handler.ResponseHandler;
 import com.mrdevv.portfolioBackend.services.IDesarrolladorService;
+import com.mrdevv.portfolioBackend.services.IExperienciaService;
 import com.mrdevv.portfolioBackend.utils.constants.TipoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +21,7 @@ import java.util.List;
 public class DesarrolladorController {
 
     private final IDesarrolladorService desarrolladorService;
+    private final IExperienciaService experienciaService;
 
     @GetMapping
     public ResponseEntity<Object> obtenerDesarrolladores(){
@@ -36,6 +39,14 @@ public class DesarrolladorController {
     public ResponseEntity obtenerDatos(){
         ResponseDesarrolladorDTO desarrolladorDTO = desarrolladorService.obtenerDatosDesarrollador();
         return ResponseHandler.ok(TipoResponse.GET, "Se obtuvo los datos del desarrollador correctamente", desarrolladorDTO);
+    }
+
+    @GetMapping("me/experiencias")
+    public ResponseEntity obtenerExperiencias(@RequestParam(name = "page", defaultValue = "0", required = false) Integer page,
+                                              @RequestParam(name = "size", defaultValue = "4", required = false) Integer size){
+        Pageable pageable = PageRequest.of(page, size);
+        ResponseWithPageable experienciasDTO = experienciaService.obtenerExperiencias(pageable);
+        return ResponseHandler.ok(TipoResponse.GETALL, "Se obtuvieron las experiencias correctamente", experienciasDTO);
     }
 
 }
--- src/main/java/com/mrdevv/portfolioBackend/repositories/ExperienciaRepository.java
+++ src/main/java/com/mrdevv/portfolioBackend/repositories/ExperienciaRepository.java
@@ -2,8 +2,11 @@ package com.mrdevv.portfolioBackend.repositories;
 
 import com.mrdevv.portfolioBackend.dto.projection.ExperienciaProjectionDTO;
 import com.mrdevv.portfolioBackend.models.Experiencia;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -18,7 +21,9 @@ public interface ExperienciaRepository extends JpaRepository<Experiencia, Long> 
             "e.nombreEmpresa, " +
             "d.nombres, " +
             "d.apellidos " +
-            "from Experiencia e join e.desarrollador d")
-    List<ExperienciaProjectionDTO> obtenerExperiencias();
+            "from Experiencia e join e.desarrollador d " +
+            "join d.usuario u where (:apiKey is null or u.apiKey = :apiKey)"
+    )
+    Page<ExperienciaProjectionDTO> obtenerExperiencias(@Param("apiKey") String apiKey, Pageable pageable);
 
 }
